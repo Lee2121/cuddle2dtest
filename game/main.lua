a0033fb95aa77e8348f3465efdedcf765035790d
Lee2121 <lcudney@yahoo.com>
2025-12-28 23:30:45 -0500

    Condensed all requires into lib.cuddle2d.init

--- game/main.lua
+++ game/main.lua
@@ -1,7 +1,4 @@
-require "lib.cuddle2d.input.inputdevicemanager"
-require "lib.cuddle2d.input.inputdefs"
-require "lib.cuddle2d.input.inputmods"
-require "lib.cuddle2d.players.playermanager"
+require "lib.cuddle2d.init"
 
 local PS4_BTN_ID_X = 1
 local PS4_BTN_ID_CIRCLE = 2
@@ -13,13 +10,15 @@ local JOYSTICK_DEADZONE = .1
 local InputContext_Test = {
 	move = InputAction_Vector2({ xaxis = { InputDef_KeyboardKey('a'), InputDef_KeyboardKey("left"), InputDef_KeyboardKey('d', InputMod_Invert()), InputDef_KeyboardKey("right", InputMod_Invert()), InputDef_GamepadAxis("leftx", InputMod_Deadzone(JOYSTICK_DEADZONE) ) },
 								 yaxis = { InputDef_KeyboardKey('w', InputMod_Invert()), InputDef_KeyboardKey("up", InputMod_Invert()), InputDef_KeyboardKey('s'), InputDef_KeyboardKey("down"), InputDef_GamepadAxis("lefty", InputMod_Deadzone(JOYSTICK_DEADZONE) ) },
-								 xyaxis = { InputDef_TouchJoystick() } } ),
+								 xyaxis = { InputDef_Touch() } } ),
 
 	jump = InputAction_Bool( { InputDef_KeyboardKey("space"), InputDef_GamepadButton(PS4_BTN_ID_SQUARE) } ),
 
 	mouseMoved = InputAction_Vector2( { xyaxis = { InputDef_MousePosition() } } ),
 
-	leftMouseClick = InputAction_Bool( { InputDef_MouseClicked(1), InputDef_KeyboardKey("return") } )
+	leftMouseClick = InputAction_Bool( { InputDef_MouseClicked(1), InputDef_KeyboardKey("return") } ),
+
+	leftScreenTouch = InputAction_Vector2( { InputDef_Touch("left") } )
 }
 
 local demoLogic = {}
@@ -49,13 +48,16 @@ end
 function love.update()
 	if demoLogic.playerInstance ~= nil then
 		local jumpvalue = demoLogic.playerInstance.inputManager:getActionValue(InputContext_Test.jump)
-		--print("jump val: ", jumpvalue)
+		print("jump val: ", jumpvalue)
 		
 		local mousevalue = demoLogic.playerInstance.inputManager:getActionValue(InputContext_Test.mouseMoved)
-		--print("mouse pos: ", mousevalue[1], mousevalue[2])
+		print("mouse pos: ", mousevalue[1], mousevalue[2])
 
 		local moveValue = demoLogic.playerInstance.inputManager:getActionValue(InputContext_Test.move)
 		print("move input", moveValue[1], moveValue[2])
+
+		local touchValue = demoLogic.playerInstance.inputManager:getActionValue(InputContext_Test.leftScreenTouch)
+		--print("move input", moveValue[1], moveValue[2])
 	end
 end
 
